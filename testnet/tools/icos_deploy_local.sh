2c9409bd9bd50a1311e2eeb215eea20f15482922
faraz <faraz.shaikh@gmail.com>
2023-03-26 18:08:34 -0700

    add static ssl directive uniformly across image canister and binaries
    Disable blanket application of bazel repin.
    Repin only when new dependancies are added

--- testnet/tools/icos_deploy_local.sh
+++ testnet/tools/icos_deploy_local.sh
@@ -53,7 +53,7 @@ fi
 cd "$(dirname "$0")"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 GIT_REVISION=$(git log --format=format:%H  --max-count=1)
-${REPO_ROOT}/gitlab-ci/container/build-ic.sh -i -d -s -b -c
+${REPO_ROOT}/gitlab-ci/container/build-ic.sh -i -b -c -d -s
 sudo rm -rf  /var/local/ic/disk/localhost/${GIT_REVISION}
 sudo mkdir -p /var/local/ic/disk/localhost/${GIT_REVISION}
 sudo cp /wrk/apk/ic/artifacts/icos/disk-img.tar.zst /var/local/ic/disk/localhost/${GIT_REVISION}/
